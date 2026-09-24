65b7c706495174211067d3b734aded2d5fa60813
Glen Coventon <glen.coventon@pertrain.com.au>
2026-09-24 10:34:26 +1000

    Update bootstap

--- gulpfile.js
+++ gulpfile.js
@@ -23,22 +23,22 @@ function browserSyncReload(done) {
   done();
 }
 
-// Clean vendor
+// Clean third-party assets
 function clean() {
-  return del(["./vendor/"]);
+  return del(["./Bootstrap/"]);
 }
 
-// Bring third party dependencies from node_modules into vendor directory
+// Bring third party dependencies from node_modules into Bootstrap directory
 function modules() {
   // Bootstrap
   var bootstrap = gulp.src('./node_modules/bootstrap/dist/**/*')
-    .pipe(gulp.dest('./vendor/bootstrap'));
+    .pipe(gulp.dest('./Bootstrap/bootstrap'));
   // jQuery
   var jquery = gulp.src([
       './node_modules/jquery/dist/*',
       '!./node_modules/jquery/dist/core.js'
     ])
-    .pipe(gulp.dest('./vendor/jquery'));
+    .pipe(gulp.dest('./Bootstrap/jquery'));
   return merge(bootstrap, jquery);
 }
 
